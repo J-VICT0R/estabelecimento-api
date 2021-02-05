b2b478c9669917ddc2918a513e29272833acf57f
Juciano Victor <jucianovictor95@gmail.com>
2021-02-05 13:02:51 -0300

    DTOs utilitários

---
+++ src/main/java/br/com/juciano/estabelecimento/model/dto/ResponseError.java
@@ -0,0 +1,15 @@
+package br.com.juciano.estabelecimento.model.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+public class ResponseError {
+
+    private String details;
+}
